0893c1b4f1d97015d887423a3a98414543b4a8f4
Michael Sanford <msanford@exou.com>
2016-07-12 16:26:35 -0400

    Beautify source according to new .editorconfig and js-beautify rules.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 8080;
 const basepath = '/bin/shared/query.php';
 
 var ping = function( req, res ) {
-  // TODO
+	// TODO
 };
 
 /**
@@ -31,123 +31,123 @@ var ping = function( req, res ) {
  * @param  {[type]} res [description]
  * @return {[type]}     [description]
  */
-var fexists = function ( req, res ) {
-  var fexists = path.join( '..', '..', req.params.fexists );
+var fexists = function( req, res ) {
+	var fexists = path.join( '..', '..', req.params.fexists );
 
-  try {
-    fs.stat( fexists, function ( err, stats ) {
-      if ( err ) {
-        console.log( "Cannot find %s: %s", fexists, err );
-        res.send( 'NOT_FOUND' );
-      } else {
-        console.log( 'Found %s', fexists );
-        res.send( 'FOUND' );
-      }
-    } );
-  } catch ( e ) {
-    console.log( "Error finding directory '%s': %s", fexists, e.message );
-  }
+	try {
+		fs.stat( fexists, function( err, stats ) {
+			if ( err ) {
+				console.log( "Cannot find %s: %s", fexists, err );
+				res.send( 'NOT_FOUND' );
+			} else {
+				console.log( 'Found %s', fexists );
+				res.send( 'FOUND' );
+			}
+		} );
+	} catch ( e ) {
+		console.log( "Error finding directory '%s': %s", fexists, e.message );
+	}
 };
 
 // POST request
-var preview = function ( req, res ) {
-  // TODO
+var preview = function( req, res ) {
+	// TODO
 };
 
-var setdocinfo = function ( req, res ) {
-  // TODO
+var setdocinfo = function( req, res ) {
+	// TODO
 };
 
-var getdocinfo = function ( req, res ) {
-  // TODO
+var getdocinfo = function( req, res ) {
+	// TODO
 };
 
 // POST
-var publish = function ( req, res ) {
-  // TODO
+var publish = function( req, res ) {
+	// TODO
 };
 
-var setcontroller = function ( req, res ) {
-  // TODO
+var setcontroller = function( req, res ) {
+	// TODO
 };
 
-var setdevicedata = function ( req, res ) {
-  // TODO
+var setdevicedata = function( req, res ) {
+	// TODO
 };
 
-var createsession = function ( req, res ) {
-  // TODO
+var createsession = function( req, res ) {
+	// TODO
 };
 
-var joinsession = function ( req, res ) {
-  // TODO
+var joinsession = function( req, res ) {
+	// TODO
 };
 
-var savegrids = function ( req, res ) {
-  // TODO
+var savegrids = function( req, res ) {
+	// TODO
 };
 
-var get_grids = function ( req, res ) {
-  // TODO
+var get_grids = function( req, res ) {
+	// TODO
 };
 
-var get_icons = function ( req, res ) {
-  // TODO
+var get_icons = function( req, res ) {
+	// TODO
 };
 
-var userpincreate = function ( req, res ) {
-  // TODO
+var userpincreate = function( req, res ) {
+	// TODO
 };
 
-var userpinadd = function ( req, res ) {
-  // TODO
+var userpinadd = function( req, res ) {
+	// TODO
 };
 
-var userpinactivate = function ( req, res ) {
-  // TODO
+var userpinactivate = function( req, res ) {
+	// TODO
 };
 
-var addpinactivate = function ( req, res ) {
-  // TODO
+var addpinactivate = function( req, res ) {
+	// TODO
 };
 
-var setuserinfo = function ( req, res ) {
-  // TODO
+var setuserinfo = function( req, res ) {
+	// TODO
 };
 
-var userinfo = function ( req, res ) {
-  // TODO
+var userinfo = function( req, res ) {
+	// TODO
 };
 
-var deletesession = function ( req, res ) {
-  // TODO
+var deletesession = function( req, res ) {
+	// TODO
 };
 
-var quitsession = function ( req, res ) {
-  // TODO
+var quitsession = function( req, res ) {
+	// TODO
 };
 
-var getlist = function ( req, res ) {
-  // TODO
+var getlist = function( req, res ) {
+	// TODO
 };
 
 // TODO was "delete" in PHP
-var deleteboard = function ( req, res ) {
-  // TODO
+var deleteboard = function( req, res ) {
+	// TODO
 };
 
-var load = function ( req, res ) {
-  // TODO
+var load = function( req, res ) {
+	// TODO
 };
 
-var errlog = function ( req, res ) {
-  // TODO
+var errlog = function( req, res ) {
+	// TODO
 };
 
 // Helper function from php.
 function get_list_for_user( userid ) {
-  // TODO
-  // req.uid
+	// TODO
+	// req.uid
 }
 
 /**
@@ -156,26 +156,26 @@ function get_list_for_user( userid ) {
  * @return {[type]}       [description]
  */
 function deleteFolder( path ) {
-  // TODO
+	// TODO
 }
 
 function sms( phone, text ) {
-  // TODO
-  // $data = file_get_contents( 'http://www.smsmatrix.com/matrix?username='.urlencode( 'jbmartinoli@exou.com' )
-  //   .
-  //   '&password='.urlencode( 'Protect128' )
-  //   .
-  //   '&phone='.urlencode( $phone )
-  //   .
-  //   '&callerid='.urlencode( '14185090580' )
-  //   .
-  //   '&txt='.urlencode( $text )
-  // );
+	// TODO
+	// $data = file_get_contents( 'http://www.smsmatrix.com/matrix?username='.urlencode( 'jbmartinoli@exou.com' )
+	//   .
+	//   '&password='.urlencode( 'Protect128' )
+	//   .
+	//   '&phone='.urlencode( $phone )
+	//   .
+	//   '&callerid='.urlencode( '14185090580' )
+	//   .
+	//   '&txt='.urlencode( $text )
+	// );
 }
 
 var server = restify.createServer( {
-  name: packagejson.name,
-  version: packagejson.version
+	name: packagejson.name,
+	version: packagejson.version
 } );
 server.use( restify.acceptParser( server.acceptable ) );
 server.use( restify.queryParser() );
@@ -184,58 +184,58 @@ server.use( restify.bodyParser() );
 /**
  * GET request router
  */
-server.get( basepath, function ( req, res, next ) {
-  res.setHeader( 'content-type', 'text/plain; charset=utf-8' );
+server.get( basepath, function( req, res, next ) {
+	res.setHeader( 'content-type', 'text/plain; charset=utf-8' );
 
-  console.log( 'Complete request parameters from : %s', JSON.stringify( req.params ) );
+	console.log( 'Complete request parameters from : %s', JSON.stringify( req.params ) );
 
-  if ( 'fexists' in req.params ) {
-    return fexists( req, res );
-  }
+	if ( 'fexists' in req.params ) {
+		return fexists( req, res );
+	}
 
-  // res.send( req.params );
+	// res.send( req.params );
 } );
 
 /**
  * POST request router
  */
-server.post( basepath, function ( req, res, next ) {
-  res.setHeader( 'content-type', 'text/plain; charset=utf-8' );
+server.post( basepath, function( req, res, next ) {
+	res.setHeader( 'content-type', 'text/plain; charset=utf-8' );
 
-  if ( 'preview' in req.params ) {
-    return preview( req, res );
-  }
+	if ( 'preview' in req.params ) {
+		return preview( req, res );
+	}
 
-  if ( 'publish' in req.params ) {
-    return publish( req, res );
-  }
+	if ( 'publish' in req.params ) {
+		return publish( req, res );
+	}
 
-  res.setHeader( 'content-type', 'text/plain' );
-  res.send( req.params );
+	res.setHeader( 'content-type', 'text/plain' );
+	res.send( req.params );
 } );
 
 /**
  * Emits health information
  */
-server.get( '/health', function ( req, res ) {
-  res.send( {
-    uptime: {
-      time: Math.floor( os.uptime() / 60 / 60 ),
-      units: "hours"
-    },
-    mem: {
-      free: Math.floor( os.freemem() / 1024 / 1024 ),
-      total: Math.floor( os.totalmem() / 1024 / 1024 ),
-      units: "mb"
-    },
-    loadavg: os.loadavg()
-  } );
+server.get( '/health', function( req, res ) {
+	res.send( {
+		uptime: {
+			time: Math.floor( os.uptime() / 60 / 60 ),
+			units: "hours"
+		},
+		mem: {
+			free: Math.floor( os.freemem() / 1024 / 1024 ),
+			total: Math.floor( os.totalmem() / 1024 / 1024 ),
+			units: "mb"
+		},
+		loadavg: os.loadavg()
+	} );
 } );
 
 /**
  * Start the server
  */
-server.listen( 8080, function () {
-  console.log( '%s listening at %s', server.name, server.url );
-  console.log( 'Environment: %s', process.env.NODE_ENV );
+server.listen( 8080, function() {
+	console.log( '%s listening at %s', server.name, server.url );
+	console.log( 'Environment: %s', process.env.NODE_ENV );
 } );
